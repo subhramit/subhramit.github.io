7c812970766c316674ba98e369d68114d2426fc6
subhramit <subhramit.bb@live.in>
2023-07-10 22:23:38 +0530

    Changed color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'uno': '#331D2C',
+        'dos': '#3F2E3E',
+        'tres': '#A78295',
+        'quad': '#EFE1D1'
+
+      }
+    },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border)-(uno|dos|tres|quad)/,
+    },
+  ],
 }
 
