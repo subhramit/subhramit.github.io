5170305c557d0e1b93f10039ad10aca1daadd204
subhramit <subhramit.bb@live.in>
2025-05-29 04:34:54 +0530

    Change vite root
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/subhramit.github.io/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
