5b33df0c3ccebd7846cbd70f4e4303c76cf365a4
subhramit <subhramit.bb@live.in>
2025-06-22 18:22:16 +0530

    Switch back to hashrouter
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
