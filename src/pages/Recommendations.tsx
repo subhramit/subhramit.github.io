2a06041f4b8286f1c6b74a6b626efee9e494a781
subhramit <subhramit.bb@live.in>
2025-06-23 23:01:45 +0530

    [Mobile] Adjust font size for recommendations
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -21,15 +21,15 @@ export default function Recommendations() {
             {/* Stand-up Comics Section */}
             <div className="text-2xl md:text-3xl text-start font-serif font-bold md:pl-60 pt-5 underline">Stand-up comics</div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
                 <p>"Dear Sad, look on the bright side. Being ghosted by your crush is far superior to being crushed by a ghost."</p>
             </div>
-            <div className="text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72">
                 <p>-Kanan Gill, April 2020</p>
             </div>
 
             <div className="text-xl md:text-2xl text-start font-serif font-medium md:pl-60 pt-2">Indian:</div>
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 md:pr-60">
                 <p>
                     <strong>1. Srijan Kaushik</strong><br />
                     <strong>2. Sumit Anand</strong><br />
@@ -42,7 +42,7 @@ export default function Recommendations() {
             </div>
 
             <div className="text-xl md:text-2xl text-start font-serif font-medium md:pl-60 pt-2">Non-Indian:</div>
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 md:pr-60">
                 <p>
                     <strong>1. Ricky Gervais</strong><br />
                     <strong>2. Zach Star</strong> (not a professional comedian but his skits are extremely funny)<br />
@@ -52,7 +52,7 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-60">
                 <p>
                     Here's a <a href="https://www.youtube.com/watch?v=YDDlFHQ1Vgw&t=468s&ab_channel=SrijanKaushik" className="hover:text-tres text-pink-600">link</a> to a short stand-up clip by Srijan Kaushik. <br />
                     Here's <a href="https://www.youtube.com/watch?v=6CdhE3hh2CQ&ab_channel=SumitAnand" className="hover:text-tres text-pink-600">one</a> by Sumit Anand. <br />
@@ -64,7 +64,7 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 md:pr-60">
                 <p>
                     <br />
                     <strong><u>Side note</u>:</strong> I also love skits by <a href="https://www.youtube.com/watch?v=R6_q0MwImTE&ab_channel=AwkwardPuppets" className="hover:text-tres text-pink-600">Awkward Puppets</a>, <a href="https://www.youtube.com/watch?v=W4EK5nvyszs&t=3s&ab_channel=AGCANDY" className="hover:text-tres text-pink-600">AGC Andy</a> (most of his content is on Instagram), and <a href="https://www.youtube.com/watch?v=OjFUJEoq8k0&ab_channel=Key%26Peele" className="hover:text-tres text-pink-600">Key and Peele</a>.
@@ -82,7 +82,7 @@ export default function Recommendations() {
                 />
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-8 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-8 md:pr-60">
                 <p>
                     <strong>1. 500 Days of Summer</strong><br />
                     <strong>2. Twelve Angry Men</strong> (Thanks to Kaustab for recommending)<br />
@@ -99,7 +99,7 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
                 <p>
                     "We wear our scarves just like a noose <br />
                     But not 'cause we want eternal sleep <br />
@@ -108,7 +108,7 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
                 <p>
                     I remember reading about the theme of 500 Days of Summer when I was going through one of my worst breakups. Discovering it, it felt like the kind of a movie that I should have avoided at that state, and should've watched sometime in future, and ideally with someone. Then one fine midnight (when I had not yet recovered), I just said to myself - "Fuck it. I'll watch it by myself. And I'll watch it right now." Little did I know, I was about to take one of the best cinematic journeys of my life so far.<br /><br />
                     I feel every Tom has had a Summer and every Summer has had a Tom at some point in their lives. The movie makes you feel every possible human emotion there is, and has absolutely beautiful cinematography and mesmerizing, cherry-picked soundtracks. It ideally takes a rewatch to completely understand the girl's perspective. Although my favouritism towards the movie could've been influenced by my situation, but even while rewatching it after recovering, I still enjoyed it as much and felt that it had a lot more to offer that I missed during my first watch.
@@ -128,7 +128,7 @@ export default function Recommendations() {
             </div>
 
             <div
-                className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-6 md:pr-60">
+                className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-6 md:pr-60">
                 <p>
                     <strong>1.Brave</strong><br />
                     <strong>2.Zootopia</strong><br />
@@ -138,14 +138,14 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
                 <p>
                     "But every once in a while, there's a day when I don't have to be a princess. No lessons, no expectations. <br />
                     A day where anything can happen. A day when I can change my fate."
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
                 <p>
                     Brave is and always will be my personal favourite. The opening monologue by Merida about fate gives me chills every time I listen to it, and takes me right back to my childhood, when I watched it the first time. I remember really enjoying the movie and don't recall crying even a bit during any scene back then. But when I watched it in college after growing up, oddly enough, I ended up shedding tears during the climax. This movie has witnessed me mature emotionally with time. <br /><br />
                     Soul is another movie that I adore a lot. In fact, if I had to recommend one Disney movie that everyone should watch, it would be Soul, as it teaches us about what real happiness truly is, and if it can ever be achieved.
@@ -155,7 +155,7 @@ export default function Recommendations() {
             {/* Series Section */}
             <div className="text-2xl md:text-3xl text-start font-serif font-bold md:pl-60 pt-6 underline">Series</div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-2 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-2 md:pr-60">
                 <p>
                     <strong>1. BoJack Horseman</strong><br />
                     <strong>2. Dark</strong><br />
@@ -166,14 +166,14 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-extralight md:pl-60 md:pr-72 pt-5 italic">
                 <p>
                     "That's the problem with life, right? Either you know what you want, and then you don't get what you want, or you get what you want, and then you don't know what you want."
                 </p>
             </div>
 
             <div
-                className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
+                className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
                 <p>
                     BoJack Horseman is the most psychologically clever series that has ever been made. It deals with topics that aren't usually talked about, and if such themes were to be portrayed by real human actors, the series would either be censored or banned. But through the animated characters and funny dialogue, you're constantly reminded - "It's a talking horse, after all - how seriously are you actually supoosed to take it?"<br /><br />
                     If you've ever reached a point of self loathing in your life, this series will hit really close to home, and resonate with all of your deepest insecurities. Not just that, it has probably the best collection of soundtracks that you'd see put together in a single show, while being very appropriately picked. Some of these songs that made it to my favourites playlist include "Los Ageless" by St. Vincent, "Break My Fall" by Doc Robinson, and "Mr. Blue" by Catherine Feeny.<br /><br />
@@ -187,7 +187,7 @@ export default function Recommendations() {
             {/* Books/Stories Section */}
             <div className="text-2xl md:text-3xl text-start font-serif font-bold md:pl-60 pt-6 underline">Books/Stories</div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-2 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-2 md:pr-60">
                 <p>
                     <strong>1. The Bet - Anton Chekhov</strong><br />
                     <strong>2. A Voice in the Dark </strong>- part of "The Autobiography of a Super-Tramp" - <strong>W. H. Davies</strong><br />
@@ -197,15 +197,15 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72">
                 <p>
                     "The Bet" is a short story that I was first introduced to in my 8th standard because of my board (ICSE). Although I did not have the maturity to, and hence wasn't able appreciate it justifiably back then, every time I re-read it at my leisure after my boards ended, my understanding of its themes deepend and I found it to be one of the most marvelous stories that have ever been written.<br />
                     It highlights the psychological changes a young lawyer goes through during a 15-year voluntary solitary confinement, as a consequence of a "stupid" bet that he takes with a rich banker to prove that life imprisonment is a more humane punishment compared to death penalty, for two million dollars. I don't wish to spoil it for you any further, but I can't keep myself from putting this quote here that you'll find relevant to the story (and this sub-section in general) after reading it (if you choose to): <br />
                 </p>
-                <div className="text-xl text-start font-serif font-extralight pl-4 md:pl-72 pr-4 md:pr-62 pt-5 italic">
+                <div className="text:lg md:text-xl text-start font-serif font-extralight pl-4 md:pl-72 pr-4 md:pr-62 pt-5 italic">
                     <p>"A reader lives a thousand lives before he dies. The man who never reads lives only one."</p>
                 </div>
-                <div className="text-xl text-start font-serif flex justify-end pl-4 md:pl-62 pr-4 md:pr-[354px]">- George R.R. Martin</div>
+                <div className="text:lg md:text-xl text-start font-serif flex justify-end pl-4 md:pl-62 pr-4 md:pr-[354px]">- George R.R. Martin</div>
                 <br />
                 <p>
                     The Bhagavad Gita is a very old scripture which consists of a conversation between two friends, Arjuna and Krishna. Arjuna finds himself in a war in which he has to fight for Dharma, against his own kin, and consequently finds himself trembling (both physically and metaphorically) and is unable to wield his bow against them. He turns to his friend Krishna for guidance, and he sings the Gita to him, which guides him through the battle out of which he emerges enlightened and victorious, despite having an army which would otherwise be considered weaker than his opponent's (Duryodhan). It is a very deep piece of literature and when unpacked properly, acts as a guide to life in general.<br /><br />
@@ -225,7 +225,7 @@ export default function Recommendations() {
                 />
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-8 md:pr-60">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-8 md:pr-60">
                 <p>
                     <strong>1. 'You Started It' with Daniel Fernandez</strong><br />
                     <strong>2. Love Aaj Kal with Aastha & Ankit</strong> - Sex, Dating and Relationships<br />
@@ -236,7 +236,7 @@ export default function Recommendations() {
                 </p>
             </div>
 
-            <div className="text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72 pb-6">
+            <div className="text-base sm:text-lg md:text-xl text-start font-serif font-medium md:pl-60 pt-4 md:pr-72 pb-6">
                 <p>
                     'You Started it' is my personal favourite because of a couple of reasons. First, the discussions are very lively and highly opinionated, especially in the first two seasons (pre-COVID era), in which there are two guests in each episode who take us through a varied range of opinions on every chosen topic. Alongside that, or you can say as a consequence, the podcast adresses a lot of socio-cultural issues that we Indians face in our day-to day lives.<br /><br />
                     'Love Aaj Kal' is a podcast in which two best friends - Ankit Vengurlekar and Aastha Atray - sit down and have a conversation about anything that's under the sun about love and dating. They navigate through topics like "Can a guy and a girl just be friends", "Is it okay to have sex on the first date", "Dating in the world of apps" and "How to identify toxic relationships", that are quite pertinent to modern-day romance and relationships.<br /><br />
