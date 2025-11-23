cb8e412d0b58b345d657099af66900303e2fa689
subhramit <subhramit.bb@live.in>
2025-11-23 18:53:46 +0530

    Add content

--- src/data/content.ts
+++ src/data/content.ts
@@ -197,7 +197,7 @@ export const blogPosts: BlogPost[] = [
   {
     title: 'The best Sunday ever',
     date: '23.11.2025',
-    content: `I got Assassin's Creed Syndicate - Gold Edition worth INR 3500 for INR 160 at the Epic Games store due to the holiday sale as well as all the Epic reward credits I had accumulated so far.
+    content: `I got myself Assassin's Creed Syndicate - Gold Edition for INR 160 (which was worth INR 3500) at the Epic Games store due to the holiday sale as well as all the Epic reward credits I had accumulated so far.
     I am over the moon.
     `,
   },
