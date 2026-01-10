0196ce072edb6620c14a4ad011a8d1e7fe076e82
subhramit <subhramit.bb@live.in>
2026-01-11 04:05:24 +0530

    Title, fix date

--- src/data/content.ts
+++ src/data/content.ts
@@ -221,8 +221,8 @@ export const blogPosts: BlogPost[] = [
     `,
   },
   {
-    title: 'What I want life to be like',
-    date: '11.01.2025',
+    title: 'Resolutions',
+    date: '11.01.2026',
     content: `Although I have always loved the idea of "new beginnings" carrying marvelous potential for change, I was never much of a believer in "new year resolutions", given their rate of success. In fact, I have never taken one in life.
     
     This year, however, I sat down to think of a few things I'd really like to do. It can certainly cause an issue when you don't love the person you are becoming, even though I feel I have left that lane long back.
